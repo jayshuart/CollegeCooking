c4a85da73eadb49be30793eb8b27376fcfa68eab
Joel Shuart <jas1785@g.rit.edu>
2016-11-13 23:40:58 -0500

    controller input

--- CollegeCooking/Assets/Scripts/Player.cs
+++ CollegeCooking/Assets/Scripts/Player.cs
@@ -7,7 +7,11 @@ using System.Collections;
 [RequireComponent (typeof (Rigidbody))]
 public class Player : MonoBehaviour 
 {
-	#region Fields
+    #region Fields
+    //input
+    InputSettings input; //input manager for this player
+    int playerNum;
+
 	//Physics
 	private Rigidbody body;
 	#endregion
@@ -21,6 +25,10 @@ public class Player : MonoBehaviour
 
 	void Start()
 	{
+        //setup input
+        input = new InputSettings(); //initialize
+        input.ConfigureInput(playerNum); //bind specific controlers input to this player
+
 		//Assign body
 		body = GetComponent<Rigidbody>();
 	}
@@ -31,11 +39,37 @@ public class Player : MonoBehaviour
 
     }
 
-    //Every update check if player is gonna fall off tile
+
     public void FixedUpdate()
     {
-        
+        //update controller input
+        input.UpdateInput();
     }
 
+    //METHODS=====================================================================================================
+    /// <summary>
+    /// moves hand based on controller input
+    /// </summary>
+    void Move()
+    {
+
+    }
+
+    /// <summary>
+    /// rotates hand based on controller input
+    /// </summary>
+    void Rotate()
+    {
+
+    }
+
+    /// <summary>
+    /// binds passed in obj to the position and roations of the hand thats grabbing it
+    /// </summary>
+    /// <param name="obj"></param>
+    void Grab(GameObject obj)
+    {
+
+    }
 }
 
