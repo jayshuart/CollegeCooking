60014386382a6ef87ea94b7f480776cd929f00d5
Nelson Scott <officialbadbeargames@gmail.com>
2016-12-12 15:00:51 -0500

    Egg yolk prefab matt use this

---
+++ CollegeCooking/Assets/Scripts/EggYolk.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EggYolk : Food 
+{
+	#region Fields
+
+	#endregion
+
+	void Start()
+	{
+		cookEvent.AddListener(Cooked);
+	}
+
+	void Cooked()
+	{
+		
+	}
+}
